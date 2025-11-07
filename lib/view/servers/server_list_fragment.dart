9dc6b17f0a85024d82b0c2e649df36d2df8586eb
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2025-11-07 20:34:36 +0300

    МАШРУТНАЯ ГОРИЗОНТАЛЬНАЯ

--- lib/view/servers/server_list_fragment.dart
+++ lib/view/servers/server_list_fragment.dart
@@ -2,22 +2,18 @@ import 'package:flutter/material.dart';
 import '../../model/server_model.dart';
 import 'screens.dart';
 
-
 class ServerListWrapper extends StatefulWidget {
   const ServerListWrapper({super.key});
-
 
   @override
   State<ServerListWrapper> createState() => _ServerListWrapperState();
 }
-
 
 class _ServerListWrapperState extends State<ServerListWrapper> {
   final List<ServerModel> _servers = [
     ServerModel(name: 'Minecraft Server 1', status: 'Онлайн', players: 12),
     ServerModel(name: 'Minecraft Server 2', status: 'Оффлайн', players: 0),
   ];
-
 
   void _addServer() {
     setState(() {
@@ -29,13 +25,11 @@ class _ServerListWrapperState extends State<ServerListWrapper> {
     });
   }
 
-
   void _removeServer() {
     setState(() {
       if (_servers.isNotEmpty) _servers.removeLast();
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -100,11 +94,10 @@ class _ServerListWrapperState extends State<ServerListWrapper> {
             ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Страничная навигация с горизонтальным свайпом
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => const HorizontalServerNavigation(),
+              builder: (context) => const RouteHorizontalServerNavigation(),
             ),
           );
         },
